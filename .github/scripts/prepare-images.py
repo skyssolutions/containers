9134c76cc949cd92c0c0c935857203d831a001ff
Skyler Mäntysaari <samip5@users.noreply.github.com>
2023-09-14 21:16:53 +0300

    Use own builders

--- .github/scripts/prepare-images.py
+++ .github/scripts/prepare-images.py
@@ -90,9 +90,9 @@ if __name__ == "__main__":
             to_append = app.copy()
             to_append["platform"] = platform
             if platform == "linux/amd64":
-                to_append["builder"] = "ubuntu-latest"
+                to_append["builder"] = "arc-sol"
             elif platform == "linux/arm64":
-                to_append["builder"] = ["self-hosted", "ARM64"]
+                to_append["builder"] = "arc-sol-arm64"
             if platform != "linux/amd64":
                 to_append["chan_tests_enabled"] = False
             out["imagePlatformPermutations"].append(to_append)
