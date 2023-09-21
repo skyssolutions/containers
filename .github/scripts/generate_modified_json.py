129b960116974e350f6d71a331f792fa0896cad6
Skyler Mäntysaari <samip5@users.noreply.github.com>
2023-09-21 17:43:41 +0300

    Heh

--- .github/scripts/generate_modified_json.py
+++ .github/scripts/generate_modified_json.py
@@ -4,12 +4,20 @@ import sys
 
 app = sys.argv[1]
 channel = sys.argv[2]
-debian_version = sys.argv[3]
+
+# Check if debian_version is provided
+try:
+    debian_version = sys.argv[3]
+except IndexError:
+    debian_version = None
 
 data = {
     'app': app,
-    'channel': channel,
-    'debian_version': debian_version
+    'channel': channel
 }
+
+# Add debian_version to data only if it's neither empty nor "null"
+if debian_version and debian_version.lower() != "null":
+    data['debian_version'] = debian_version
 
 print(json.dumps(data, separators=(',', ':')))
