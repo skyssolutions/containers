a6877c8b97ae5d184d725354537765f546cfe468
Skyler Mäntysaari <samip5@users.noreply.github.com>
2023-12-08 12:11:45 +0200

    Updates

--- .github/scripts/upstream.sh
+++ .github/scripts/upstream.sh
@@ -3,6 +3,8 @@
 APP="${1}"
 CHANNEL="${2}"
 
-if test -f "./apps/${APP}/ci/latest.sh"; then
+if test -f "./apps/${APP}/ci/latest.py"; then
+    python ./apps/"${APP}"/ci/latest.py
+elif test -f "./apps/${APP}/ci/latest.sh"; then
     bash ./apps/"${APP}"/ci/latest.sh "${CHANNEL}"
 fi
---
+++ apps/caddy-cf/ci/latest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import requests
+import re
+from packaging import version
+
+def get_latest_caddy_version():
+    # Fetch tags from Docker Hub
+    response = requests.get("https://registry.hub.docker.com/v2/repositories/library/caddy/tags?ordering=last_updated")
+    tags = response.json()['results']
+
+    # Filter out tags that follow semantic versioning (major.minor.patch)
+    version_tags = [tag['name'] for tag in tags if re.match(r'^\d+\.\d+(\.\d+)?$', tag['name'])]
+
+    # Sort the versions
+    sorted_versions = sorted(version_tags, key=lambda x: version.parse(x), reverse=True)
+
+    # Return the latest version
+    return sorted_versions[0] if sorted_versions else None
+
+latest_version = get_latest_caddy_version()
+print(latest_version)
