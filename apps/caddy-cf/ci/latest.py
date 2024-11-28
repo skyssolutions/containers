34cd041b317dabd4c4f1318b01a0b5ed89c06470
Skyler Mäntysaari <samip5@users.noreply.github.com>
2024-11-28 12:30:31 +0200

    caddy: Exclude beta versions (#64)

--- apps/caddy-cf/ci/latest.py
+++ apps/caddy-cf/ci/latest.py
@@ -16,19 +16,14 @@ def get_all_tags():
 
 def get_latest(channel_name):
     tags = get_all_tags()
-    #print([tag['name'] for tag in tags])  # Print all tag names for debugging
-
-    # Modify regex to extract versions from tag names
-    version_tags = [re.search(r'\d+\.\d+(\.\d+)?', tag['name']).group(0)
-                    for tag in tags if re.search(r'\d+\.\d+(\.\d+)?', tag['name'])]
-    #print(version_tags)  # Print extracted version tags for debugging
-
-    # Sort the versions
+    # Modify regex to exclude beta versions
+    version_tags = [re.search(r'\d+\.\d+\.\d+', tag['name']).group(0)
+                    for tag in tags if re.search(r'\d+\.\d+\.\d+', tag['name']) and '-beta' not in tag['name']]
     sorted_versions = sorted(version_tags, key=lambda x: version.parse(x), reverse=True)
     # print(sorted_versions)  # Print sorted versions for debugging
 
     # Return the latest version
     return sorted_versions[0] if sorted_versions else None
 
-#latest_version = get_latest("stable")
-#print(latest_version)
+# latest_version = get_latest("stable")
+# print(latest_version)
