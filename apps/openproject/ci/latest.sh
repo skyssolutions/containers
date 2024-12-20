3fa07586ed251835b570d00c307a5643aa8f8fb0
Skyler Mäntysaari <sm+git@skym.fi>
2024-12-20 21:06:08 +0200

    Hmm

--- apps/openproject/ci/latest.sh
+++ apps/openproject/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-LATEST_VERSION=$(curl -s https://api.github.com/repos/opf/openproject/releases/latest | jq -r .tag_name)
+LATEST_VERSION=$(curl -s https://api.github.com/repos/opf/openproject/releases/latest | jq -r .tag_name | sed 's/^v//') # Remove leading "v"
 
 echo $LATEST_VERSION
