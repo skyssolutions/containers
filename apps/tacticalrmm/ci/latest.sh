d6bba5a886e0b997ff9c79cbc44f9f1c570442fb
Skyler Mäntysaari <samip5@users.noreply.github.com>
2026-09-11 19:52:51 +0300

    Delete apps/tacticalrmm

--- apps/tacticalrmm/ci/latest.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/amidaware/tacticalrmm/releases/latest" | jq --raw-output '.tag_name')"
-version="${version#*v}"
-printf "%s" "${version}"
